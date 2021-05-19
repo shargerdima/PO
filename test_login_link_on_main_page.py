49250404e17855595a563a030eaa3bbd17031fea
shargerdima <36707042+shargerdima@users.noreply.github.com>
2021-05-19 22:38:49 +0300

    Create test_login_link_on_main_page.py

---
+++ test_login_link_on_main_page.py
@@ -0,0 +1,14 @@
+from pages.MainPage import MainPage
+
+
+def test_guest_should_see_login_link(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)
+    page.open()
+    page.should_be_login_link()
+
+def test_guest_should_see_login_link_with_bug(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)
+    page.open()
+    page.should_be_login_link_bug()
